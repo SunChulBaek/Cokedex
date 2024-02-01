6cd8d20405aeb4dea953b018bbc665a64028536d
x1210x <x1210x@gmail.com>
2024-02-01 10:56:29 +0900

    update workflow

--- buildSrc/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ buildSrc/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -23,8 +23,8 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
                 configureKotlinAndroid(this)
 
                 defaultConfig {
-                    applicationId = properties.getProperty("applicationId")
-                    targetSdk = properties.getProperty("targetSdk").toInt()
+                    applicationId = "kr.pe.ssun.cokedex"
+                    targetSdk = 34
                     versionCode = properties.getProperty("versionCode").toInt()
                     versionName = properties.getProperty("versionName")
                 }
